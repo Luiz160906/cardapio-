abc3170675e218a21e18ea3ae13ab6b04779d4f9
Luiz160906 <felipeluiz412@gmail.com>
2024-06-14 19:55:46 -0300

    final código

--- codigo.py
+++ codigo.py
@@ -87,7 +87,82 @@ def alterar(categoria, item_atual, item_novo):
             print(f"Item '{item_atual}' alterado para '{item_novo}' com sucesso.")
         else:
             print(f"O item '{item_atual}' não existe na categoria '{categoria}'.")
-            if opcao == 4: 
+
+
+def menup():
+    while True:
+        print('******* MENU *******')
+        print('* 1 Adicionar itens *')
+        print('* 2 Excluir Itens *')
+        print('* 3 Alterar itens do cardápio*')
+        print('* 4  Buscar itens *')
+        print('* 5 listar todos os itens do cardápio *')
+        print('* 6 encerrar programa *')
+        print('****************')
+        opcao = int(input("Digite o número da opção desejada: "))
+        if opcao == 1:
+            print('******* MENU *******')
+            print('* 1. Adicionar entradas *')
+            print('* 2. Adicionar prato principal *')
+            print('* 3. Adicionar sobremesas*')
+            print('* 4. Adiconar bebidas *')
+            print('****************')
+            opcaoadd = int(input("Digite o numero da opção da categoria desejada: "))
+            if opcaoadd == 1:
+                quant = int(input("Quantas entradas você quer adicionar: "))
+                for i in range(quant):
+                  new = str(input("Digite o item que você deseja adicionar:"))
+                  neww = float(input("Digite o valor do novo item: "))
+                  entradas[new]= neww
+            elif opcaoadd == 2:
+                quant = int(input("Quantos pratos principais você quer adicionar: "))
+                for i in range(quant):
+                  newp = str(input("Digite o item que você deseja adicionar:"))
+                  newwp = float(input("Digite o valor do novo item: "))
+                  pratop[newp]= newwp
+            elif opcaoadd == 3:
+                quant = int(input("Quantas sobremesas você quer adicionar: "))
+                for i in range(quant):
+                  news = str(input("Digite o item que você deseja adicionar:"))
+                  newws = float(input("Digite o valor do novo item: "))
+                  sobremesa[news]= newws
+            elif opcaoadd == 4:
+                    quant = int(input("Quantas bebidas você quer adicionar: "))
+                    for i in range(quant):
+                        sub = str(input("Digite a subcategoria do item: "))
+                        newb = str(input("Digite o item que você deseja adicionar:"))
+                        newwb = float(input("Digite o valor do novo item: "))
+                        if sub in bebidas:
+                            bebidas[sub][newb] = newwb
+                        else:
+                            bebidas[sub] = {newb: newwb}
+                            print(f"Subcategoria: {sub} criada ")
+            print(f"O cardapio atualizado ficou assim: {cardapio}")
+            
+
+        if opcao !=1 or opcao !=2 or opcao !=3 or opcao !=4 or opcao !=5 or opcao !=6:
+            print('digite uma opção já existente')
+
+        if opcao == 2:
+            categoria_ = input("Categoria: ")
+            item_ = input("Item: ")
+            deletar(categoria_, item_)
+            print(cardapio)
+            menup()
+
+
+
+
+        elif opcao == 3:
+            categoria_ = input("Digite a categoria do item que deseja alterar: ").strip().lower()
+            item_atual = input("Digite o nome atual do item: ").strip().title()
+            item_novo = input("Digite o novo nome do item: ").strip().title()
+            alterar(categoria_, item_atual, item_novo)
+
+        
+        
+
+        if opcao == 4: 
             categoria_ = input("Digite a categoria do item que deseja buscar: ").lower()
             item_ = input("Digite o nome do item que deseja buscar: ").lower()
             retorno = get_key(categoria_, item_)
